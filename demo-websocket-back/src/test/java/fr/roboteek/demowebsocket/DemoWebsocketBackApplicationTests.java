1397befd85b36a81d0e6c2bcc6c69d4ea3e1ef74
npeltier <nico.peltier@gmail.com>
2021-02-23 17:11:20 +0100

    Commit structure

---
+++ demo-websocket-back/src/test/java/fr/roboteek/demowebsocket/DemoWebsocketBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.roboteek.demowebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebsocketBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
